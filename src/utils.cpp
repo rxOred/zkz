a3a958619465d73d5fd41a7c5aed5e75bf6f42ab
rxOred <redriotezpz@gmail.com>
2021-07-25 23:19:28 +0000

    printhelp && printusage

--- src/utils.cpp
+++ src/utils.cpp
@@ -7,16 +7,16 @@
 
 void PrintHelp(void){
 
-    log.Print("[zkz] help\nQuick navigation help for zkz debugger\n\t
-            [r]un / [c]ontinue\tstart debugging\n\t
-            list\tlist line information\n\t
-            [b]reak\tbreakpoint at memory address\n\t
-            [b]reak[l]\tbreakpoint at line number\n\t
-            select\tselect a compilation unit\n\t
-            [[i]nfo] [[r]egisters]\tregister information\n\t
-            [[d]el]ete\tdelete a breakpoint\n\t
-            set\tset register values\n\t
-            [dis]able\t disable breakpoints\n\t
+    log.Print("[zkz] help\nQuick navigation help for zkz debugger\n\t \
+            [r]un / [c]ontinue\tstart debugging\n\t             \
+            list\tlist line information\n\t                     \
+            [b]reak\tbreakpoint at memory address\n\t           \
+            [b]reak[l]\tbreakpoint at line number\n\t           \
+            select\tselect a compilation unit\n\t               \
+            [[i]nfo] [[r]egisters]\tregister information\n\t    \
+            [[d]el]ete\tdelete a breakpoint\n\t                 \
+            set\tset register values\n\t                        \
+            [dis]able\t disable breakpoints\n\t                 \
             exit\texit zkz");
 }
 
