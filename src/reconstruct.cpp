838c938d1aaa43bb7742997b803e50b4e4c6940e
rxOred <rxored@gmail.com>
2021-08-10 02:16:28 +0000

    TODO

--- src/reconstruct.cpp
+++ src/reconstruct.cpp
@@ -4,6 +4,24 @@
 #include <sys/types.h>
 
 #include "log.h"
-#include "bin.h"
+#include "elfp.h"
 
-int Elf::ReadTextSegment(pid_t pid, )
+/*
+ * reconstruct damaged/stripped dynamically linked binary files
+ */
+
+/*
+ * first read the text segment and extract it (.plt located in this segment)
+ */
+
+/*
+ * read and extract data segment (GOT located in this segment)
+ */
+
+/*
+ * program header table can be used to do this task
+ */
+
+/*
+ * locate GOT in data segment using dynamic segment->d_tag==DT_PLTGOT
+ */
