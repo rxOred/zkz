9230eb7f0ac27fa9abc01faa90e124997a2aa78b
rxOred <rxored@gmail.com>
2021-08-19 01:17:53 +0000

    changes to be commited

--- src/reconstruct.cpp
+++ src/reconstruct.cpp
@@ -41,47 +41,12 @@ failed:
 }
 
 /*
- * reconstruct damaged/stripped dynamically linked binary files
- */
-
-/*
  * first read the text segment and extract it 
  * (.plt located in this segment)
  */
 
 int Reconstruct::ReadTextSegment(void)
 {
-    uint64_t v_addr = 0;
-    size_t v_sz = 0;
-
-    char proc_path[STR_SZ];
-    char addr_buf[ADDR_SZ];
-    std::string line;
-
-    sprintf(proc_path, MAP_PATH, m_pid);
-    std::ifstream proc(proc_path, std::ios::in | std::ios::binary);
-    if(proc.is_open()){
-        while(std::getline(proc, line, ' ')){
-            char prev_c = 0;
-            for(int i = 0; i < line.length(); i++){
-                if(line[i] == 'r' && line[i+1] == '-' && line[i+2] ==
-                        'x'){
-                    for(int j = 0; j < 16; j++){
-                        addr_buf[j] = line[j];
-                        std::sscanf(addr_buf, "%lx", &v_addr);
-                        if(Process::pread(m_pid, m_seg_text, v_addr, 
-                                    v_sz) < 0)
-                            goto file_failed;
-                    }
-                }
-            }
-        }
-    } else 
-        goto failed;
-
-file_failed:
-    proc.close();
-
 failed:
     return -1;
 }
@@ -90,37 +55,8 @@ failed:
  */
 int Reconstruct::ReadDataSegment(void)
 {
-    uint64_t v_addr = 0;
-    size_t v_sz = 0;
-
-    char proc_path[STR_SZ];
-    char addr_buf[ADDR_SZ];
-    std::string line;
-
-    sprintf(proc_path, MAP_PATH, m_pid);
-    std::ifstream proc(proc_path, std::ios::in | std::ios::binary);
-    if(proc.is_open()){
-        while(std::getline(proc, line, ' ')){
-            char prev_c = 0;
-            for(int i = 0; i < line.length(); i++){
-                if(line[i] == 'r' && line[i+1] == 'r' && line[i+2] ==
-                        '-'){
-                    for(int j = 0; j < 16; j++){
-                        addr_buf[j] = line[j];
-                        std::sscanf(addr_buf, "%lx", &v_addr);
-                        if(Process::pread(m_pid, m_seg_data, v_addr, 
-                                    v_sz) < 0)
-                            goto file_failed;
-                    }
-                }
-            }
-        }
-    } else 
-        goto failed;
-
-file_failed:
-    proc.close();
-
+    Segdata *seg = Process::get_segment_data("rw-", m_pid)
+    if(seg == nullptr)
 failed:
     return -1;
 }
