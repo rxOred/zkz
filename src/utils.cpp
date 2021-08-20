ef7e9392f03d7eaeffc3acbd131fd1d666134791
rxOred <rxored@gmail.com>
2021-08-20 22:59:55 +0000

    minor change

--- src/utils.cpp
+++ src/utils.cpp
@@ -8,24 +8,24 @@
 void PrintHelp(void){
 
     log.Print("[zkz] help\nQuick navigation help for zkz debugger\n\t \
-            [r]un / [c]ontinue\tstart debugging\n\t             \
-            list\tlist line information\n\t                     \
-            [b]reak\tbreakpoint at memory address\n\t           \
-            [b]reak[l]\tbreakpoint at line number\n\t           \
-            select\tselect a compilation unit\n\t               \
-            [[i]nfo] [[r]egisters]\tregister information\n\t    \
-            [[d]el]ete\tdelete a breakpoint\n\t                 \
-            set\tset register values\n\t                        \
-            [dis]able\t disable breakpoints\n\t                 \
+            [r]un / [c]ontinue\tstart debugging\n             \
+            list\tlist line information\n             \
+            [b]reak\tbreakpoint at memory address\n           \
+            [b]reak[l]\tbreakpoint at line number\n           \
+            select\tselect a compilation unit\n               \
+            [[i]nfo] [[r]egisters]\tregister information\n    \
+            [[d]el]ete\tdelete a breakpoint\n                 \
+            set\tset register values\n                        \
+            [dis]able\t disable breakpoints\n                 \
             exit\texit zkz");
 }
 
 void PrintUsage(void){
 
-    log.Print("[zkz] Usage\nQuick help to start zkz debugger\n\t \
-            -f - Start executing given file as the debugee\n\t  \
-            -p - Attach to given process ID\n\t                 \
-            -s - Enable system call tracing\n\t                 \
-            -i - Print register information on each step\n\t    \
+    log.Print("[zkz] Usage\nQuick help to start zkz debugger\n \
+            -f - Start executing given file as the debugee\n  \
+            -p - Attach to given process ID\n               \
+            -s - Enable system call tracing\n                      \
+            -i - Print register information on each step\n    \
             ");
 }
